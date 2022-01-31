3d81b4f5df758de311459afce5ecc4a4a02d4c9a
Joker3551 <MACHINIMA96@MAIL.RU>
2022-02-01 06:55:29 +1000

    dz1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Open Sans', sans-serif;
+                font-family: 'Montserrat';
+            }
+        </style>
+    </head>
+    <body>
+        <script>
+            console.log("Hello World!");
+        </script>
+
+        <H1>Hello World!<H1>
+    </body>
+</html>
